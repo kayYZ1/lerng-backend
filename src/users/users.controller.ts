19cae205efd8cbdc608215cb0bb1f40e6ad99ec6
kayYZ1 <overek7133@gmail.com>
2024-03-11 21:01:20 +0100

    feat: use @GetCurrId in user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,20 @@
-import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
+
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create.dto';
 import { UpdateUserDto } from './dto/update.dto';
+
+import { ATGuard } from '../common/guards/accessToken.guard';
+import { GetCurrId } from '../common/decorators/getCurrId.decorator';
 
 @Controller('users')
 export class UsersController {
@@ -12,8 +25,10 @@ export class UsersController {
     return this.usersService.createUser(dto);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
-    return this.usersService.updateUser(+id, dto);
+  @UseGuards(ATGuard)
+  @HttpCode(HttpStatus.OK)
+  @Patch('/update')
+  update(@GetCurrId() id: number, @Body() dto: UpdateUserDto) {
+    return this.usersService.updateUser(id, dto);
   }
 }
