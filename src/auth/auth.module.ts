ca0a273019f98a3d9b404b278d7c560fea87f76a
kayYZ1 <overek7133@gmail.com>
2024-03-11 20:42:57 +0100

    fix(auth): add strategies to providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,9 @@ import { JwtModule } from '@nestjs/jwt';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from '../users/users.module';
+import { AccessTokenStrategy } from './strategies/access-token.strategy';
+import { RefreshTokenStrategy } from './strategies/refresh-token.strategy';
 
 @Module({
   imports: [
@@ -13,6 +15,6 @@ import { UsersModule } from 'src/users/users.module';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AccessTokenStrategy, RefreshTokenStrategy],
 })
 export class AuthModule {}
