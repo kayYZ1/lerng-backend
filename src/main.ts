e52c4d03db0fe763a37d4aa4ce9fae772ec311da
Kacper <overek7133@gmail.com>
2024-08-26 11:50:20 +0200

    feat: add netlify origin

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173', 'https://lerng.netlify.app/',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: [
       'Content-Type',
