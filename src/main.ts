f89819ce4e451516b2e9844ebb238f38bd3c13f4
Kacper <overek7133@gmail.com>
2024-08-26 14:31:42 +0200

    fix(cors): add credentials

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
       'Authorization',
     ],
     exposedHeaders: ['Authorization'],
-    credentials: false,
+    credentials: true,
   });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
